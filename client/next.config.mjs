93544d9efbef9ecb60a47f45ab7d75687704297d
Poteritesh <riteshpote0603@gmail.com>
2026-07-29 11:50:43 +0530

    Db update

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -1,8 +1,18 @@
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   allowedDevOrigins: ['195.35.21.96'],
-
   reactCompiler: true,
+  experimental: {
+    turbopack: {
+      root: __dirname,
+    },
+  },
 };
 
 export default nextConfig;
